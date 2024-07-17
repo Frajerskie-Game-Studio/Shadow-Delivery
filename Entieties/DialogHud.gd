a1eaf72b19724082a7efab0338a962131d7886df
PVT-Kubi <jast.kuba@gmail.com>
2024-07-17 23:38:25 +0200

    Adding documentation

--- Entieties/DialogHud.gd
+++ Entieties/DialogHud.gd
@@ -8,13 +8,15 @@ signal next_line
 
 func _ready():
 	pass
-	
+
+#function loading data to dialog
 func load_data(npc_name, npc_text):
 	dialog_text = npc_text
 	$MarginContainer/HBoxContainer/Panel/VBoxContainer/MarginContainer/Name.text = npc_name
 	$MarginContainer/HBoxContainer/Panel/VBoxContainer/MarginContainer2/RichTextLabel.text = dialog_text
 	writetext = true
 
+#this nice animation of dialog text
 func _process(delta):
 	if writetext:
 		if $MarginContainer/HBoxContainer/Panel/VBoxContainer/MarginContainer2/RichTextLabel.visible_characters < dialog_text.length():
@@ -22,5 +24,6 @@ func _process(delta):
 		else:
 			$MarginContainer/HBoxContainer/Panel/VBoxContainer/MarginContainer2/RichTextLabel.visible_characters = -1
 			writetext = false
+	#emmiting signal to load next dialog line
 	elif Input.is_action_just_pressed("mouse_click"):
 		next_line.emit()
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+#speed of player
 var speed = 300.0
+#variable blocking player movement
 var can_move = true
 
 func get_input():
@@ -9,19 +11,23 @@ func get_input():
 	var directionX = 0
 	var directionY = 0
 	
+	#setting Y axis in player movement vector if player is not moving in X axis
 	if Input.get_action_strength("move_right") - Input.get_action_strength("move_left") == 0 and Input.get_action_strength("move_down") - Input.get_action_strength("move_up") != 0 :
 		directionY =  Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	#setting X axis in player movement vector if player is not moving in Y axis
 	elif Input.get_action_strength("move_right") - Input.get_action_strength("move_left") != 0 and Input.get_action_strength("move_down") - Input.get_action_strength("move_up") == 0:
 		directionX = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
+	#setting player speed
 	if Input.is_action_pressed("run"):
 		speed = 380.0
 	else:
 		speed = 300.0
-		
+	#returning player movement vector
 	return Vector2(directionX, directionY)
 
 func _physics_process(delta):
+	#moving player
 	if can_move:
 		var directionVector = get_input()
 		velocity = directionVector.normalized() * speed
