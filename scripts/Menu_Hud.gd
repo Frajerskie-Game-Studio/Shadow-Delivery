bc9875ffae0b0f52164982a84b47de861c385fa3
PVT-Kubi <jast.kuba@gmail.com>
2024-07-18 13:54:13 +0200

    Adding items menu

--- scripts/Menu_Hud.gd
+++ scripts/Menu_Hud.gd
@@ -13,12 +13,40 @@ func load_profiles(teammates):
 		
 		$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Profiles .add_child(new_profile)
 
+func add_items():
+	for item in Items:
+		var key = item.keys()[0]
+		$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Items/ItemList.add_item(str(key).replace("_", " "))
+
 func _ready():
 	var text = FileAccess.get_file_as_string("res://Data/party_data.json")
 	var temp_data = JSON.parse_string(text)
 	Items = temp_data["items"]
 	load_profiles(temp_data["teammates"])
-	
+	add_items()
 
 func _process(delta):
 	pass
+
+func unshowCards():
+	$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Items.visible = false
+	$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Profiles.visible = false
+
+func _on_item_list_item_clicked(index, at_position, mouse_button_index):
+	var item = Items[index]
+	$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Items/HBoxContainer/ItemDesc.text = item[item.keys()[0]][0]
+	$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Items/HBoxContainer/ItemAmmount.text = "Ammount: " + str(item[item.keys()[0]][3])
+
+
+func _on_item_list_item_activated(index):
+	print("active")
+
+
+func _on_button_pressed():
+	if $Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Items.visible:
+		unshowCards()
+		$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Profiles.visible = true
+	else:
+		unshowCards()
+		$Control/MarginContainer/HBoxContainer/Panel/MarginContainer/Items.visible = true
+		
