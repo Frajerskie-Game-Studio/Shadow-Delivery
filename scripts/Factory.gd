b4056a78c125290c95a60965a040986cde5a5bd9
PVT-Kubi <jast.kuba@gmail.com>
2024-07-27 01:30:13 +0200

    Designing factory

---
+++ scripts/Factory.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var deletedPointers = []
+
+signal start_dialog(path)
+signal start_npc_dialog(npc_name, dialog_dict, dialog_npc, action)
+
+func load_data(data):
+	deletedPointers = data.deleted_pointers
+	for child in $Dialogs.get_children():
+		if child.Deletable and deletedPointers.find(child.name) != -1:
+			child.queue_free()
+
+func _ready():	
+	get_parent().switch_zoom(3.13,3.13)
+	#$Dialogs/DeskDialog.load_data("res://Data/desk.json", true, false, null, true)
+	#$Dialogs/BedDialog.load_data("res://Data/wake_up_dialog.json", false, true, null, false)
+	#$Dialogs/DoorDialog.load_data("res://Data/door_dialog.json", true, false, get_parent().switch_level, false)
+
+func _process(delta):
+	pass
+
+
+func _on_desk_dialog_start_dialog(path, d, action):
+	start_dialog.emit(path, d, action)
+	if deletedPointers.find(d.name) == -1 and d.Deletable:
+		deletedPointers.append(d.name)
+
+func _on_npc_show_dialog(npc_name, dialog_dict, dialog_npc, action):
+	start_npc_dialog.emit(npc_name, dialog_dict, dialog_npc, action)
+
